e01878efeb93e1f16a746c540dc2d55417b4c731
thienleggtr <letrungthien78911@gmail.com>
2025-08-12 14:21:14 +0700

    WaterMark.java

--- src/main/java/thunder/hack/features/hud/impl/WaterMark.java
+++ src/main/java/thunder/hack/features/hud/impl/WaterMark.java
@@ -54,7 +54,7 @@ public class WaterMark extends HudElement {
         if (mode.getValue() == Mode.Big) {
             Render2DEngine.drawHudBase(context.getMatrices(), getPosX(), getPosY(), 106, 30, HudEditor.hudRound.getValue());
             FontRenderers.thglitch.drawString(context.getMatrices(), "THUNDERHACK", getPosX() + 5.5, getPosY() + 5, -1);
-            FontRenderers.monsterrat.drawGradientString(context.getMatrices(), "recode", getPosX() + 35.5f, getPosY() + 21f, 1);
+            FontRenderers.monsterrat.drawGradientString(context.getMatrices(), "skibidi", getPosX() + 35.5f, getPosY() + 21f, 1);
             setBounds(getPosX(), getPosY(), 106, 30);
         } else if (mode.getValue() == Mode.Small) {
             if (HudEditor.hudStyle.is(HudEditor.HudStyle.Blurry)) {
